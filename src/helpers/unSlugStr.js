9cfb40a9cf2a5e024728d89c325c7a095341975d
Amirrk79 <amirmntmntmntmnt@gmail.com>
2022-07-27 20:27:58 +0430

    resolve comments

--- src/helpers/unSlugStr.js
+++
@@ -1,7 +0,0 @@
-const { default: capitalizeFirstLetter } = require('./capitalizeFirstLetter');
-
-function unSlugStr(str) {
-  return capitalizeFirstLetter(str.split('-').join(' '));
-}
-
-export default unSlugStr;
